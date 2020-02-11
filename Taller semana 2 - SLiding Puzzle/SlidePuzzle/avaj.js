dd0b1444dcd689f7d3a9d40a977fcfd0886bc431
juanheri <juanheri@unisabana.edu.co>
2020-02-10 22:34:07 -0500

    Adjuntando talleres 1 y 2

---
+++ Taller semana 2 - SLiding Puzzle/SlidePuzzle/avaj.js
@@ -0,0 +1,234 @@
+var negro = 9;
+
+function cambiar(x) {
+  switch (x) {
+    case 1:
+      if (negro == 2) {
+        document.getElementById("img2").src = document
+          .getElementById("img1")
+          .getAttribute("src");
+        document.getElementById("img1").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 1;
+      }
+      if (negro == 4) {
+        document.getElementById("img4").src = document
+          .getElementById("img1")
+          .getAttribute("src");
+        document.getElementById("img1").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 1;
+      }
+
+      break;
+
+    case 2:
+      if (negro == 1) {
+        document.getElementById("img1").src = document
+          .getElementById("img2")
+          .getAttribute("src");
+        document.getElementById("img2").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 2;
+      }
+      if (negro == 3) {
+        document.getElementById("img3").src = document
+          .getElementById("img2")
+          .getAttribute("src");
+        document.getElementById("img2").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 2;
+      }
+      if (negro == 5) {
+        document.getElementById("img5").src = document
+          .getElementById("img2")
+          .getAttribute("src");
+        document.getElementById("img2").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 2;
+      }
+
+      break;
+
+    case 3:
+      if (negro == 2) {
+        document.getElementById("img2").src = document
+          .getElementById("img3")
+          .getAttribute("src");
+        document.getElementById("img3").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 3;
+      }
+      if (negro == 6) {
+        document.getElementById("img6").src = document
+          .getElementById("img3")
+          .getAttribute("src");
+        document.getElementById("img3").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 3;
+      }
+
+      break;
+
+    case 4:
+      if (negro == 1) {
+        document.getElementById("img1").src = document
+          .getElementById("img4")
+          .getAttribute("src");
+        document.getElementById("img4").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 4;
+      }
+      if (negro == 5) {
+        document.getElementById("img5").src = document
+          .getElementById("img4")
+          .getAttribute("src");
+        document.getElementById("img4").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 4;
+      }
+      if (negro == 7) {
+        document.getElementById("img7").src = document
+          .getElementById("img4")
+          .getAttribute("src");
+        document.getElementById("img4").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 4;
+      }
+
+      break;
+
+    case 5:
+      if (negro == 2) {
+        document.getElementById("img2").src = document
+          .getElementById("img5")
+          .getAttribute("src");
+        document.getElementById("img5").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 5;
+      }
+      if (negro == 4) {
+        document.getElementById("img4").src = document
+          .getElementById("img5")
+          .getAttribute("src");
+        document.getElementById("img5").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 5;
+      }
+      if (negro == 6) {
+        document.getElementById("img6").src = document
+          .getElementById("img5")
+          .getAttribute("src");
+        document.getElementById("img5").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 5;
+      }
+      if (negro == 8) {
+        document.getElementById("img8").src = document
+          .getElementById("img5")
+          .getAttribute("src");
+        document.getElementById("img5").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 5;
+      }
+
+      break;
+
+    case 6:
+      if (negro == 3) {
+        document.getElementById("img3").src = document
+          .getElementById("img6")
+          .getAttribute("src");
+        document.getElementById("img6").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 6;
+      }
+      if (negro == 5) {
+        document.getElementById("img5").src = document
+          .getElementById("img6")
+          .getAttribute("src");
+        document.getElementById("img6").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 6;
+      }
+      if (negro == 9) {
+        document.getElementById("img9").src = document
+          .getElementById("img6")
+          .getAttribute("src");
+        document.getElementById("img6").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 6;
+      }
+
+      break;
+
+    case 7:
+      if (negro == 4) {
+        document.getElementById("img4").src = document
+          .getElementById("img7")
+          .getAttribute("src");
+        document.getElementById("img7").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 7;
+      }
+      if (negro == 8) {
+        document.getElementById("img8").src = document
+          .getElementById("img7")
+          .getAttribute("src");
+        document.getElementById("img7").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 7;
+      }
+
+      break;
+
+    case 8:
+      if (negro == 5) {
+        document.getElementById("img5").src = document
+          .getElementById("img8")
+          .getAttribute("src");
+        document.getElementById("img8").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 8;
+      }
+      if (negro == 7) {
+        document.getElementById("img7").src = document
+          .getElementById("img8")
+          .getAttribute("src");
+        document.getElementById("img8").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 8;
+      }
+      if (negro == 9) {
+        document.getElementById("img9").src = document
+          .getElementById("img8")
+          .getAttribute("src");
+        document.getElementById("img8").src =
+          "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+        negro = 8;
+      }
+
+      break;
+
+    case 9:
+      if (negro != 9) {
+        if (negro == 6) {
+          document.getElementById("img6").src = document
+            .getElementById("img9")
+            .getAttribute("src");
+          document.getElementById("img9").src =
+            "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+          negro = 9;
+        }
+        if (negro == 8) {
+          document.getElementById("img8").src = document
+            .getElementById("img9")
+            .getAttribute("src");
+          document.getElementById("img9").src =
+            "https://eska.com/wp-content/uploads/2019/09/eska-mono-black-voor.jpg";
+          negro = 9;
+        }
+      }
+      break;
+  }
+}
