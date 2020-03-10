f5e138aeeb3594db403f2e9a83f42bafbfd75969
jukens1998 <juken.555@hotmail.com>
2020-03-10 10:18:00 -0500

    To do list

---
+++ To do list/precache-manifest.2b192616df121e03f7dbcd340f1e9d12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7257af1d88549fd96a0ec3bf001ab20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdc00bcf3b808106163",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7dbdc7414a764e334b87",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbdc00bcf3b808106163",
+    "url": "/static/js/2.dadbebbb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.dadbebbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbdc7414a764e334b87",
+    "url": "/static/js/main.8b2e5e1c.chunk.js"
+  },
+  {
+    "revision": "7b7468d93faf1c645299",
+    "url": "/static/js/runtime-main.7fb465a4.js"
+  }
+]);
