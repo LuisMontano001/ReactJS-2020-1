5ecd301f1bd9206780666d2c835cc5b94887d0d6
Andres <andrew241@outlook.es>
2020-03-16 14:44:12 -0500

    FreshRestart

--- Taller 4 - CSS Modules/cssmodules/src/App.js
+++ Taller 4 - CSS Modules/cssmodules/src/App.js
@@ -1,50 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import classes from './Components/App.css';
-import InsertBar from './Components/InsertBar';
-import Task from './Components/Task';
-import DoneTask from './Components/DoneTask';
-class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      tasks:[],
-      doneTasks:[],
-      newTask:[]
-    }
-  }
-  handleChange=(e)=>{
-    var value=e.target.value;
-    this.setState({newTask:[value,"unDone"]})
-  }
-  handleClick=(e)=>{
-    var newTask = this.state.newTask
-    this.setState({tasks:[...this.state.tasks, newTask]})
-  }
-  handleClick2=(e)=>{
-    var doneTask= [this.state.newTask[0],"done"]
-    this.setState({doneTasks:[...this.state.doneTasks, doneTask]})
-    console.log(doneTask)
-  }
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div id='mainDiv'>
-            <InsertBar handleChange={this.handleChange} handleClick={this.handleClick} />  
-            <div className="columns"> 
-              <h2>Tasks</h2>
-              {this.state.tasks.map((task, index)=><Task task={task} key={index} handleClick={this.handleClick2} />)}
-            </div>
-            <div className="columns">
-              <h2>Done</h2>
-              {this.state.doneTasks.map((task, index)=><DoneTask task={task} key={index} handleClick={this.handleClick2} />)}
-            </div>
-          </div>
-        </header>
-      </div>
-    );
-  }
-  
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
