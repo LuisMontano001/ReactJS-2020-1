37fc175ee57c4037f7e7dee620a4ec8cad8bf49d
Cristian Paredes <cristiandparedesc@gmail.com>
2020-03-17 18:59:04 -0500

    http requests: get, post and delete methods added to app

--- http-requests/src/Components/FullPost/FullPost.js
+++ http-requests/src/Components/FullPost/FullPost.js
@@ -3,10 +3,11 @@ import classes from './FullPost.css';
 import { withRouter } from 'react-router-dom';
 
 var FullPost = (props) => {
-    var openPost = props.openPost(props.match.params.postIndex);
+    var postIndex = props.match.params.postIndex;
+    var openPost = props.openPost(postIndex);
 
     return (
-        <div className = {classes["full-post"]}>
+        <div className = {classes["full-post"]} onClick = {() => props.removePost(postIndex)} >
             <h2>{openPost.title}</h2>
             <p>{openPost.content}</p>
             <p className = {classes["author-text"]}>{openPost.author}</p>
