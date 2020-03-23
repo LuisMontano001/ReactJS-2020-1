1cb842c88d8f83bd493f05fe755a277f963ecd63
jukens1998 <juken.555@hotmail.com>
2020-03-23 14:21:39 -0500

    Taller segundo corte peticiones HTTP

---
+++ HTTTP/precache-manifest.f9eafa758a36a85e09f02dfe02d551ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c5cebf644ced1a3db8c7e959142ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb135063271bb8f4db1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cfd9d9f4d77e20049d2f",
+    "url": "/static/js/2.4338d8c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4338d8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb135063271bb8f4db1",
+    "url": "/static/js/main.e454ea00.chunk.js"
+  },
+  {
+    "revision": "f77a4bf80f697fc610a5",
+    "url": "/static/js/runtime-main.f5f0d2c3.js"
+  }
+]);
